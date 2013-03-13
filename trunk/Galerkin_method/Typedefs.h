9626b68aacf633a683a8e22399ef46efde287d7e
c.dovgal@gmail.com <c.dovgal@gmail.com@3834b825-0dd7-888e-8b51-f9c449f9da55>
2013-03-13 00:03:53 +0000

    add Runge_Kutta_solver from previous project

---
+++ trunk/Galerkin_method/Typedefs.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <vector>
+
+#include "boost/numeric/ublas/matrix.hpp"
+#include "boost/numeric/ublas/vector.hpp"
+#include "boost/numeric/ublas/io.hpp"
+
+typedef boost::numeric::ublas::vector<double> Vector;
+typedef boost::numeric::ublas::matrix<double> Matrix;
