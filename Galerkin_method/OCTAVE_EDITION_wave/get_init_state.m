62c2924a3357c720d1c36762ab7cc2751b5584c6
c.dovgal <c.dovgal@gmail.com>
2013-04-24 10:24:18 +0000

    -

--- Galerkin_method/OCTAVE_EDITION_wave/get_init_state.m
+++ Galerkin_method/OCTAVE_EDITION_wave/get_init_state.m
@@ -1,12 +1,13 @@
 function init_state = get_init_state(number)
+
 	dx = 1/number;
 	phi = @(x) sin(pi*x) + 0.5*sin(2*pi*x) + 0.25*sin(4*pi*x);
 	psi = @(x) pi*(cos(pi*x) + cos(2*pi*x) + cos(4*pi*x));
 	
-	init_state = zeros(1,number*2);
+	init_state = zeros(1,(number+1)*2);
 	
-	for i = 1:number
-		init_state(i) = 	   phi((i-1)*dx);
+	for i = 1:number+1
+    init_state(i)        = phi((i-1)*dx);
 		init_state(i+number) = psi((i-1)*dx);
 	endfor
 	
